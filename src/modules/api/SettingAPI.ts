f6f7fa093b7fb1f1d003d04f41317785b31a8961
Kim JiYong <znzld505@naver.com>
2021-02-24 16:48:41 +0900

    commit

--- src/modules/api/SettingAPI.ts
+++ src/modules/api/SettingAPI.ts
@@ -1,6 +1,5 @@
-// export const APIURL = "http://192.168.215.8:8000/api/v1";
-// export const APIURL2 = "http://192.168.215.8:8000/api/v1";
+export const APIURL = "http://192.168.215.8:8000/api/v1";
+export const APIURL2 = "http://192.168.215.8:8000/api/v1";
 
-
-export const APIURL = "http://10.111.20.133:8080/api/v1";
-export const APIURL2 = "http://10.111.20.134:8080/api/v1";
+// export const APIURL = "http://10.111.20.133:8080/api/v1";
+// export const APIURL2 = "http://10.111.20.134:8080/api/v1";
